1fd10724445a230717268caab3e96d9721113324
FredrikJonsall <fredrik.jonsall@edu.sundsvall.se>
2025-10-07 15:18:31 +0200

    provkod version 2
    enligt rubrik

---
+++ provkod_ver2/provkod_ver2.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode(5, OUTPUT);
+
+  pinMode(7, INPUT_PULLUP);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (!digitalRead(7)) {
+
+    analogWrite(5, 78);
+
+    Serial.println("Don't Touch Me!");
+
+    delay(200);
+
+  } else {
+
+    digitalWrite(8, LOW);
+  }
+}
