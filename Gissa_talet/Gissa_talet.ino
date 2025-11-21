ce72a85a87392a0446178b4223c18c0d1205482c
FredrikJonsall <fredrik.jonsall@edu.sundsvall.se>
2025-11-21 16:14:53 +0100

    Projekt Gissa Talet!
    Färdigt(?) Gissa talet program!

---
+++ Gissa_talet/Gissa_talet.ino
@@ -0,0 +1,73 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int ratt = A0;
+int knapp = 2;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  pinMode(knapp, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int hemligtTal = random(0, 99);  // tal mellan 0 och 100
+  int forsok = 0;                  // antalet gissningar som gjorts
+  int gissning;
+  bool harVunnit = false;
+
+  lcd.setCursor(0, 0);
+  lcd.print("Gissa ett nummer");
+  lcd.setCursor(0, 1);
+  lcd.print("mellan 0 och 99");
+  delay(250);
+
+  while (forsok < 5 && harVunnit == false) {
+    while (digitalRead(knapp)) {
+      gissning = map(analogRead(ratt), 0, 1023, 0, 99);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Gissning: ");
+      lcd.print(gissning);
+      lcd.setCursor(0, 1);
+      lcd.print("Antal forsok: ");
+      lcd.print(forsok);
+      delay(250);
+    }
+    delay(300);
+    forsok = forsok + 1;  // forsok +=1;   forsok++;
+    if (gissning == hemligtTal) {
+      harVunnit = true;
+    } else if (gissning < hemligtTal) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Talet ar storre!");
+    } else {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Talet ar mindre!");
+    }
+  }
+  if (harVunnit == false) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DU MISSLYCKADES!");
+    lcd.setCursor(0, 1);
+    lcd.print("Talet var: ");
+    lcd.print(hemligtTal);
+    delay(3000);
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DU VANN! Tal:");
+    lcd.print(hemligtTal);
+    lcd.setCursor(0, 1);
+    lcd.print("Antal gissn: ");
+    lcd.print(forsok);
+    delay(3000);
+  }
+
+  delay(250);  // medan vi prövar oss fram
+}
